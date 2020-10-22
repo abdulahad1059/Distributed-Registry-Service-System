e4d52ea24ca941a1f00db4aea4dcd19f66248d2c
abdulahad1059 <52977368+abdulahad1059@users.noreply.github.com>
2020-10-22 18:12:19 +0530

    Update main.js

--- DS_PROJECT/RPC-Implementation-Code/RPC-Client/main.js
+++ DS_PROJECT/RPC-Implementation-Code/RPC-Client/main.js
@@ -1,10 +1,10 @@
 let proxy = require('./stub/main');		 
 async function main() {
-    let addResult = await proxy.callProcedure('add', 'number', 4, 1);
+    let addResult = await proxy.callProcedure('add', 'int', 4, 1);
     console.log('Addition result 4+1 => ' + addResult); 
-    let subResult = await proxy.callProcedure('sub', 'number', 3, 5);
+    let subResult = await proxy.callProcedure('sub', 'int', 3, 5);
     console.log('Subtraction result 3-5 => ' + subResult);
-    let mulResult = await proxy.callProcedure('mul', 'number', 1, 2);
+    let mulResult = await proxy.callProcedure('mul', 'int', 1, 2);
     console.log('Multiplication result 1*2 => ' + mulResult);
 }
 main();
